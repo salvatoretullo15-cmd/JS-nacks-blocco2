81ab70d249b9602e2c6bd69d6fad5240ca75869b
salvatore tullo <salvatoretullo15@gmail.com>
2026-02-05 16:57:26 +0100

    commit finale

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 //chiedere all'utente di inserire due parole in successione con due prompt.
 //qui lutente inserisce la sua prima parola 
-const firstWord=(prompt("write a word"))
+const first_Word=prompt("write a word")
 //qui l'utente inserisce la sua seconda parola 
-const secondWord=(prompt("write a word"))
+const second_Word=prompt("write a another word")
 
-if(firstWord>secondWord){
-    console.log("la parola piu corta è:",firstWord);
-} else if (secondWord>firstWord){
-    console.log("la parola piu lunga è:",secondWord)
+if(second_Word.length > first_Word.length){
+    message=`the second word is longer than the first ${second_Word} ${first_Word}`;
+} else if (first_Word.length > second_Word.length){
+    message==`the first word is longer than the second ${first_Word} ${second_Word}`;
+} else {
+    message =`the word are equal ${first_Word} ${second_Word}`;
 }
-console.log(firstWord)
+console.log(message);
